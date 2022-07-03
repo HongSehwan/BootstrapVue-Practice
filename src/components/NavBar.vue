4a570d487977187460d864fb231a064e22f2997f
Hong <gg9297@gmail.com>
2022-07-03 22:07:08 +0900

    Add Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse" />
+      
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/#">공지사항</b-nav-item>
+          <b-nav-item href="/notice">게시판</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data() {
+    return {};
+  }
+}
+</script>
